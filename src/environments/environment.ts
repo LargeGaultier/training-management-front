f24be85fdbb6644e729b6d0d87279ae2b80bca66
Thomas Blondet <thomas.blondet@cgi.com>
2019-10-12 12:35:09 +0200

    Added refblock route env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,8 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
-  production: false
+  production: false,
+  apiTrainingCenter:
+    'https://trainingmanagementrestapi.azurewebsites.net/api/v1/training-center/',
+  routeRefBlocks: 'refblocks/'
 };
 
 /*
@@ -13,4 +12,4 @@ export const environment = {
  * This import should be commented out in production mode because it will have a negative impact
  * on performance if an error is thrown.
  */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+import 'zone.js/dist/zone-error'; // Included with Angular CLI.
