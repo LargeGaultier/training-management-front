1f233a1f33a9754b3212aa2bbd450ec935352314
Thomas Blondet <thomas.blondet@cgi.com>
2019-10-12 16:45:34 +0200

    Add training center state

--- src/app/training-center/training-center.actions.ts
+++ src/app/training-center/training-center.actions.ts
@@ -1,3 +1,14 @@
+import { RefBlock } from '../models/refBlock.model';
+
 export class GetRefBlocks {
-  static readonly type = '[training-center] get RefBlocks';
+  static readonly type = '[TrainingCenter Page] GetRefBlocks';
+}
+
+export class GetRefBlocksSuccess {
+  static readonly type = '[TrainingCenter Page] GetRefBlocksSuccess';
+  constructor(public payload: RefBlock[]) {}
+}
+
+export class GetRefBlocksFail {
+  static readonly type = '[TrainingCenter Page] GetRefBlocksFail';
 }
--- src/app/training-center/training-center.state.ts
+++ src/app/training-center/training-center.state.ts
@@ -1,25 +1,57 @@
 import { Action, State, StateContext } from '@ngxs/store';
+import { catchError, tap } from 'rxjs/operators';
 import { RefBlock } from '../models/refBlock.model';
 import { CourseService } from './course/providers/course.service';
-import { GetRefBlocks } from './training-center.actions';
+import {
+  GetRefBlocks,
+  GetRefBlocksFail,
+  GetRefBlocksSuccess
+} from './training-center.actions';
 
 export interface TrainingCenterModel {
-  loading: boolean;
+  loadingRefBlocks: boolean;
   refBlocks: RefBlock[];
+  isloadingRefBlocksFailed: boolean;
 }
 
 @State<TrainingCenterModel>({
   name: 'training-center',
   defaults: {
-    loading: false,
-    refBlocks: []
+    loadingRefBlocks: false,
+    refBlocks: [],
+    isloadingRefBlocksFailed: false
   }
 })
 export class TrainingCenterState {
   constructor(private courseService: CourseService) {}
+
   @Action(GetRefBlocks)
-  getRefBlocks({ patchState }: StateContext<TrainingCenterModel>) {
-    patchState({ loading: true });
-    return this.courseService.getRefBlocks(0).pipe();
+  getRefBlocks({ dispatch, patchState }: StateContext<TrainingCenterModel>) {
+    patchState({ loadingRefBlocks: true });
+    return this.courseService
+      .getRefBlocks(0)
+      .pipe(
+        tap(
+          res => dispatch(new GetRefBlocksSuccess(res)),
+          catchError(() => dispatch(GetRefBlocksFail))
+        )
+      );
+  }
+
+  @Action(GetRefBlocksSuccess)
+  getRefBlocksSuccess(
+    { patchState }: StateContext<TrainingCenterModel>,
+    { payload }: GetRefBlocksSuccess
+  ) {
+    patchState({
+      loadingRefBlocks: false,
+      isloadingRefBlocksFailed: false,
+      refBlocks: payload
+    });
+  }
+
+  @Action(GetRefBlocksFail)
+  getRefBlockFail({ patchState }: StateContext<TrainingCenterModel>) {
+    patchState({ loadingRefBlocks: false, isloadingRefBlocksFailed: true });
   }
 }
