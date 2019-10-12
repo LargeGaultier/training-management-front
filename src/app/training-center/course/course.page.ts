75c875ac790565706ed20a85921565c8a7b92241
Thomas Blondet <thomas.blondet@cgi.com>
2019-10-12 12:34:19 +0200

    Added course page and service

---
+++ src/app/training-center/course/course.page.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'trm-course',
+  templateUrl: './course.page.html',
+  styleUrls: ['./course.page.scss']
+})
+export class CoursePage {}
--- src/app/training-center/training-center.module.ts
+++ src/app/training-center/training-center.module.ts
@@ -1,10 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { CoursePage } from './course/course.page';
+import { CourseService } from './course/providers/course.service';
 import { TrainingCenterRoutingModule } from './training-center-routing.module';
 import { TrainingCenterPage } from './training-center.page';
 
 @NgModule({
-  declarations: [TrainingCenterPage],
-  imports: [CommonModule, TrainingCenterRoutingModule]
+  declarations: [TrainingCenterPage, CoursePage],
+  imports: [CommonModule, TrainingCenterRoutingModule],
+  providers: [CourseService]
 })
 export class TrainingCenterModule {}
